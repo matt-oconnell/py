2b46b0136005cfd92dd8e14ea89daaf61d682c54
matt-oconnell <mattoconnell408@gmail.com>
2017-04-30 21:34:03 -0400

    Add some temp data storage

--- spotify-playlists/main.py
+++ spotify-playlists/main.py
@@ -3,20 +3,25 @@ import spotify_utils
 
 app = Flask(__name__)
 
+
 @app.route('/playlists/')
 @app.route('/playlists/<username>')
 def playlists(username=None):
     if not username:
         return 'Username required! Example: playlists/my_username'
-    playlists = spotify_utils.get_user_playlists(username)
-    return render_template('playlists.html', user=username, playlists=playlists)
+    playlists_list = spotify_utils.get_user_playlists(username)
+    return render_template('playlists.html', user=username, playlists=playlists_list)
+
 
 @app.route('/playlist')
 def playlist():
     user_id = request.args.get('user_id')
     playlist_id = request.args.get('playlist_id')
     playlist_data = spotify_utils.get_user_playlist(user_id, playlist_id)
-    return render_template('playlist.html', count=len(playlist_data['tracks']), name= playlist_data['name'], tracks = playlist_data['tracks'])
+    return render_template('playlist.html',
+                           count=len(playlist_data['tracks']),
+                           name=playlist_data['name'],
+                           tracks=playlist_data['tracks'])
 
 
 if __name__ == "__main__":
--- spotify-playlists/spotify_utils.py
+++ spotify-playlists/spotify_utils.py
@@ -1,27 +1,34 @@
 import os
+import pickle
 import spotipy
+import keys
 from spotipy.oauth2 import SpotifyClientCredentials
 
-import keys
+DATA_DIR = 'data'
 
-os.environ['SPOTIPY_CLIENT_ID'] = keys.SPOTIPY_CLIENT_ID
-os.environ['SPOTIPY_CLIENT_SECRET'] = keys.SPOTIPY_CLIENT_SECRET
+client_credentials_manager = SpotifyClientCredentials(
+    keys.SPOTIPY_CLIENT_ID,
+    keys.SPOTIPY_CLIENT_SECRET
+)
 
-client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
-def get_user_playlists(username):
-    retval = []
-    playlists = sp.user_playlists(username)
-    while playlists:
-        for i, playlist in enumerate(playlists['items']):
-            retval.append(playlist)
-        if playlists['next']:
-            playlists = sp.next(playlists)
-        else:
-            playlists = None
-    return retval
+def get_data_filename(filename):
+    return os.path.join(os.path.dirname(__file__), '{}/{}'.format(DATA_DIR, filename))
+
+
+def get_pickle_or_store(filename, callback):
+    file_path = get_data_filename(filename)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as input_file:
+            data = pickle.load(input_file)
+    else:
+        data = callback()
+        with open(file_path, 'wb') as f:
+            pickle.dump(data, f)
+    return data
+
 
 def append_tracks(tracks, arr):
     for i, item in enumerate(tracks['items']):
@@ -29,15 +36,29 @@ def append_tracks(tracks, arr):
         arr.append(track)
 
 
+def get_user_playlists(username):
+    ret_val = []
+    filename = username + '-playlists.pickle'
+    playlists = get_pickle_or_store(filename, lambda: sp.user_playlists(username))
+
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            ret_val.append(playlist)
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+    return ret_val
+
+
 def get_user_playlist(user_id, playlist_id):
-    playlist = sp.user_playlist(user_id, playlist_id, fields="tracks,next,name")
-    ret_name = playlist['name']
+    filename = user_id + '-' + playlist_id + '.pickle'
+    playlist = get_pickle_or_store(filename, lambda: sp.user_playlist(user_id, playlist_id, fields="tracks,next,name"))
+
     tracks = playlist['tracks']
     ret_tracks = []
     append_tracks(playlist['tracks'], ret_tracks)
     while tracks['next']:
         tracks = sp.next(tracks)
         append_tracks(tracks, ret_tracks)
-    return { 'tracks': ret_tracks, 'name': ret_name }
-
-
+    return {'tracks': ret_tracks, 'name':  playlist['name']}
